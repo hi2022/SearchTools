1bf71983bc41ed323015591133ccda55cbfae3fc
SysAdmiN <97897361+syslinks@users.noreply.github.com>
2023-01-28 09:55:40 +0100

    Add files via upload
    update start.bat downloads

---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off
+echo SearchDb X SearchTools
+echo       Download
+
+@echo -----------------------------------
+color a
+title SearchDb X SearchTools Install
+@echo Choix 1 [1] Installer Searchdb X SearchTools
+@echo Choix 2 [2] Exit 
+set /p choix=  Choisis La Methode Que Tu Souhaite. : 
+if %choix%==1 goto Download
+if %choix%==2 goto Exit
+:Download
+start chrome https://cdn.discordapp.com/attachments/1068635223711035544/1068636401190576128/SearchUpdate2.7z
+exit
+goto tache Terminer
+:Exit
+exit
+pause
+goto Tache Terminer
